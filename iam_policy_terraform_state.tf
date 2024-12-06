f535ec4aece39ba8e58b9b791450b0b6e9cda262
Shu Kutsuzawa <cappyzawa@gmail.com>
2024-12-06 10:33:48 +0900

    enable to lock state with s3 native

--- iam_policy_terraform_state.tf
+++ iam_policy_terraform_state.tf
@@ -27,3 +27,16 @@ data "aws_iam_policy_document" "put_terraform_state" {
     actions   = ["s3:PutObject"]
   }
 }
+
+data "aws_iam_policy_document" "lock_terraform_state" {
+  statement {
+    resources = ["arn:aws:s3:::${var.s3_bucket_terraform_state_name}/*.tflock"]
+    actions   = ["s3:PutObject", "s3:DeleteObject"]
+  }
+}
+
+resource "aws_iam_policy" "lock_terraform_state" {
+  count  = var.s3_bucket_terraform_state_name == "" ? 0 : 1
+  name   = "GitHubActions_Terraform_${var.name}_lock_terraform_state"
+  policy = data.aws_iam_policy_document.lock_terraform_state.json
+}
