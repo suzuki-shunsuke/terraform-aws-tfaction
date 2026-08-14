3cb8aca3573d483e0765182e66f62acb5d09bac7
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2026-08-14 09:15:59 +0900

    feat: support S3 Buckets encrypted with customer managed keys (#629)
    * feat: support S3 Buckets encrypted with customer managed keys
    
    If S3 Buckets for Terraform State and tfmigrate history are encrypted with
    customer managed keys, IAM Roles need permissions of the KMS Keys as well as
    the permissions of the buckets, but this module granted only the latter.
    
    Add the variables s3_bucket_terraform_state_kms_key_arns and
    s3_bucket_tfmigrate_history_kms_key_arns to grant kms:Decrypt and
    kms:GenerateDataKey to the IAM Roles which need them.
    
    * docs: fix the rationale for the list type variables
    
    An unknown value in a `dynamic` block's `for_each` doesn't fail the plan.
    Terraform defers the block, and the whole aws_iam_policy_document with it,
    to apply time. Only `count` and `for_each` of a resource fail with an
    unknown value.
    
    The actual reasons for the list type are that several KMS Keys can be
    granted, and that the statements are still rendered in the plan when the
    ARN is unknown.
    
    * refactor: extract the conditions of the KMS statements to local values
    
    The same condition was repeated in five dynamic blocks.
    
    Also fix the rationale for the list type variables again. The plan doesn't
    show the policies even with lists, because aws_iam_policy.policy is a single
    JSON string and an unknown element makes the whole string unknown. The
    remaining reason is that several KMS Keys can be granted.

--- locals.tf
+++ locals.tf
@@ -1,4 +1,10 @@
 locals {
+  # `for_each` of the `dynamic` blocks granting permissions of the KMS Keys.
+  # `length()` is known at plan time even if the elements are unknown, such as ARNs of KMS Keys
+  # created in the same plan.
+  terraform_state_kms   = length(var.s3_bucket_terraform_state_kms_key_arns) == 0 ? [] : [1]
+  tfmigrate_history_kms = length(var.s3_bucket_tfmigrate_history_kms_key_arns) == 0 ? [] : [1]
+
   default_assume_role_policy_main_conditions = [
     {
       test     = "StringEquals"
--- variables.tf
+++ variables.tf
@@ -20,6 +20,21 @@ variable "s3_bucket_terraform_state_name" {
   default = ""
 }
 
+# These variables are lists rather than strings so that several KMS Keys can be granted, for
+# instance while migrating from one KMS Key to another, where objects encrypted with the old key
+# still have to be decrypted.
+variable "s3_bucket_terraform_state_kms_key_arns" {
+  type        = list(string)
+  default     = []
+  description = "ARNs of KMS Keys encrypting the S3 Bucket for Terraform State. If the bucket is encrypted with customer managed keys, you have to set this variable so that IAM Roles can read and write Terraform State."
+}
+
+variable "s3_bucket_tfmigrate_history_kms_key_arns" {
+  type        = list(string)
+  default     = []
+  description = "ARNs of KMS Keys encrypting the S3 Bucket for tfmigrate history. If the bucket is encrypted with customer managed keys, you have to set this variable so that IAM Roles can read and write tfmigrate history."
+}
+
 variable "create_oidc_provider" {
   type    = bool
   default = false
